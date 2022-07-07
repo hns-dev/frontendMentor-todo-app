6a16c4a1b19546b434d86b1c6baa542e344e4254
hns-dev <4thewin4441@gmail.com>
2022-07-07 17:27:58 +0300

    Connect to the database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-// Listen to port
-app.listen(process.env.PORT, (req, res) => {
-  console.log("Listening for requests");
-});
+// Connect to the database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to db & Listening for requests");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
