47857a465b7e7255fd141f9d08eada8028c47ad2
hns-dev <4thewin4441@gmail.com>
2022-07-13 06:16:31 +0300

    Delete compoleted todos from the database

--- src/components/ClearCompletedTodos.js
+++ src/components/ClearCompletedTodos.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { useTodosContext } from "../hooks/useTodosContext";
 
-export default function ClearCompletedTodos({ onCleareCompleted }) {
+export default function ClearCompletedTodos() {
+  const { dispatch } = useTodosContext();
+
+  const handleCleareCompletedTodos = async () => {
+    const response = await fetch("/api/todos", {
+      method: "DELETE",
+    });
+
+    const json = await response.json();
+
+    if (response.ok) dispatch({ type: "DELETE_COMPLETED_TODOS" });
+  };
+
   return (
     <button
       className="btn btn-clear-completed-todos surface-color padding-y"
-      onClick={() => onCleareCompleted()}
+      onClick={() => handleCleareCompletedTodos()}
     >
       Clear Completed
     </button>
